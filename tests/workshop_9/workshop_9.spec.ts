422fa9fc4d74a666cce44007527cdaf5215f9b89
szm1013 <szilagyi.mihaly1013@gmail.com>
2026-08-08 13:25:03 +0200

    workshop 9 workflows 3

--- tests/workshop_9/workshop_9.spec.ts
+++ tests/workshop_9/workshop_9.spec.ts
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test';
 
-test.only('Automating Form Submissions @githubAction', async ({ page }) => {
+test('Automating Form Submissions @githubAction', async ({ page }) => {
     await page.goto('https://demo.playwright.dev/todomvc/#/');
 
     const newTodo = await page.getByPlaceholder('What needs to be done?');
@@ -21,7 +21,7 @@ test.only('Automating Form Submissions @githubAction', async ({ page }) => {
 })
 
 
-test.only('Handling Form @githubAction', async ({ page }) => {
+test('Handling Form @githubAction', async ({ page }) => {
     await page.goto('https://demo.playwright.dev/todomvc/#/');
     await page.fill('[placeholder="What needs to be done?"]', 'John Doe');
     await page.locator('[placeholder="What needs to be done?"]').press('Enter');
