975eec46598be73e661fbe03e022f0803f0de7fd
szm1013 <szilagyi.mihaly1013@gmail.com>
2026-08-11 14:03:44 +0200

    workshop 10

---
+++ tests/workshop_10/workshop_10.spec.ts
@@ -0,0 +1,31 @@
+import {test, expect} from '@playwright/test';
+
+test.skip('Interact with elements', async ({page}) => {
+
+    await page.goto('https://demo.playwright.dev/todomvc/#/');
+
+    
+
+})
+
+test.skip('Screenshot', async ({page}) => {
+    await page.goto('https://demo.playwright.dev/todomvc/#/');
+    await page.screenshot({path: 'fail.png'});
+    await page.goto('https://demo.playwright.dev/todomvc/#/');
+});
+
+test.only('Flaky', async ({page}) => {
+    await page.goto('https://demo.playwright.dev/todomvc/#/');
+
+
+
+    page.on('response', (response) => {
+        console.log(`Received from ${response.url()}: ${response.status()}`);
+    });
+
+    const flaky = Math.random() < 0.5;
+    if (flaky) {
+        await page.waitForTimeout(1000);
+        await page.click('non-existing-element');
+    }
+});
